f81850062b97e21247f8135c5d1d4e49c42e3105
avinash-developer <avicawasthi@gmail.com>
2024-04-06 16:40:23 +0530

    Prototype V1 Ready

--- app/src/main/java/com/example/navindoor/RegisterActivity.kt
+++ app/src/main/java/com/example/navindoor/RegisterActivity.kt
@@ -1,18 +1,24 @@
 package com.example.navindoor
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler
 import com.amazonaws.services.cognitoidentityprovider.model.SignUpResult
-import android.provider.Settings
+
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
@@ -30,26 +36,36 @@ class RegisterActivity : AppCompatActivity() {
             return
         }
 
+        // Check if it's the first time opening the register activity
+        if (!isPermissionsGranted()) {
+            // Show the permissions dialog
+            showPermissionsDialog()
+        }
+
         val signUpButton: Button = findViewById(R.id.signUpButton)
         val usernameEditText: EditText = findViewById(R.id.usernameEditText)
         val passwordEditText: EditText = findViewById(R.id.passwordEditText)
         val nameEditText: EditText = findViewById(R.id.nameEditText)
-        val enableNearbyPermissionsButton: Button = findViewById(R.id.enableNearbyPermissionsButton)
+     //   val enableNearbyPermissionsButton: Button = findViewById(R.id.enableNearbyPermissionsButton)
         val continueWithoutRegisterButton: Button = findViewById(R.id.continueWithoutRegisterButton)
-        val turnOnLocationButton: Button = findViewById(R.id.turnOnLocationButton)
+    //    val turnOnLocationButton: Button = findViewById(R.id.turnOnLocationButton)
 
         continueWithoutRegisterButton.setOnClickListener {
-            startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
-            finish()
+            if (isPermissionsGranted()) {
+                navigateToMainActivity()
+            } else {
+                // Show permissions dialog if permissions are not granted
+                showPermissionsDialog()
+            }
         }
 
-        enableNearbyPermissionsButton.setOnClickListener {
-            // Open the app settings screen
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            val uri = Uri.fromParts("package", packageName, null)
-            intent.data = uri
-            startActivity(intent)
-        }
+//        enableNearbyPermissionsButton.setOnClickListener {
+//            // Open the app settings screen
+//            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+//            val uri = Uri.fromParts("package", packageName, null)
+//            intent.data = uri
+//            startActivity(intent)
+//        }
 
         signUpButton.setOnClickListener {
             val username = usernameEditText.text.toString()
@@ -67,8 +83,12 @@ class RegisterActivity : AppCompatActivity() {
 
                     runOnUiThread {
                         Toast.makeText(applicationContext, "Data saved", Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
-                        finish()
+                        if (isPermissionsGranted()) {
+                            navigateToMainActivity()
+                        } else {
+                            // Show permissions dialog if permissions are not granted
+                            showPermissionsDialog()
+                        }
                     }
                 }
 
@@ -80,14 +100,79 @@ class RegisterActivity : AppCompatActivity() {
                 }
             })
         }
-        turnOnLocationButton.setOnClickListener {
-            // Open the location settings screen
-            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-            startActivity(intent)
-        }
+//        turnOnLocationButton.setOnClickListener {
+//            // Open the location settings screen
+//            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//            startActivity(intent)
+//        }
 
 
     }
+    private fun showPermissionsDialog() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Permissions Required")
+            .setMessage("To use this app, you need to enable nearby permissions and turn on location.")
+            .setPositiveButton("Enable Nearby Permissions") { dialog, _ ->
+                openAppSettings()
+                dialog.dismiss()
+            }
+            .setNegativeButton("Turn On Location") { dialog, _ ->
+                openLocationSettings()
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    private fun openLocationSettings() {
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun isNearbyPermissionsGranted(): Boolean {
+        val bluetoothPermission = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.BLUETOOTH
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val bluetoothAdminPermission = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.BLUETOOTH_ADMIN
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val fineLocationPermission = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val coarseLocationPermission = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        return bluetoothPermission && bluetoothAdminPermission && fineLocationPermission && coarseLocationPermission
+    }
+
+
+    private fun isPermissionsGranted(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as? LocationManager
+        val locationEnabled = locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) ?: false
+        val nearbyPermissionsGranted = isNearbyPermissionsGranted()
+
+        return locationEnabled && nearbyPermissionsGranted
+
+    }
+    private fun navigateToMainActivity() {
+        startActivity(Intent(this@RegisterActivity, MainActivity::class.java))
+        finish()
+    }
+
 
 
 
